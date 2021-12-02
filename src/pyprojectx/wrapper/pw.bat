6ff13cd715ff9dfea5d1c2daf5e846ce615fb0d8
ihoubr <ivo.houbrechts@nike.com>
2021-12-02 16:43:20 +0100

    fix github action command on windows

--- src/pyprojectx/wrapper/pw.bat
+++ src/pyprojectx/wrapper/pw.bat
@@ -1,5 +1,6 @@
-@echo off
-echo ==========
-echo %~dp0\pw %*
-echo ==========
+if exist %~dp0\pw (
+    echo ==== file exists ====
+) else (
+    echo ==== file doesn't exist ====
+)
 python %~dp0\pw %*
