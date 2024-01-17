64c70f330c825af09f52a7d238b9c468aa03cace
ihoubr <ivo.houbrechts@nike.com>
2024-01-17 10:33:00 +0100

    closes #70: fix windows build (take 4)

--- tests/integration/test_pw.py
+++ tests/integration/test_pw.py
@@ -234,7 +234,7 @@ locked_requirements = {
         "post-install": "mkdir post-install-table-dir",
     },
 }
-if sys.platform.startswith("win"):
+if not sys.platform.startswith("win"):
     locked_requirements["tool-with-known-requirements"]["requirements"].remove("colorama==0.4.6")
 
 
