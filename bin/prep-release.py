7f14480416bb1e50b4c179f9e1958b8cd0192f33
ihoubr <ivo.houbrechts@nike.com>
2024-01-29 18:11:41 +0100

    improve release process

--- bin/prep-release.py
+++ bin/prep-release.py
@@ -1,36 +1,65 @@
-import os
 import re
+import subprocess
+import sys
+from datetime import UTC, datetime
 from pathlib import Path
 from zipfile import ZipFile
 
-# extract version from tag
-RELEASE_VERSION = os.environ["GITHUB_REF_NAME"]
 
-# cleanup old files
-Path("wrappers.zip").unlink(missing_ok=True)
-Path(".changelog.md").unlink(missing_ok=True)
+def main(release_version):
+    cleanup_old_files()
+    generate_release_changelog()
+    replace_version_in_files(release_version)
+    zip_wrappers()
+    update_changelog(release_version)
+    subprocess.run(["git", "tag", release_version], check=True)
 
-# replace __version__ in wrapper and pyproject.toml
-pw = Path("src/pyprojectx/wrapper/pw.py")
-pw.write_text(pw.read_text().replace("__version__", RELEASE_VERSION))
-pyproject = Path("pyproject.toml")
-pyproject_content = re.sub(r'version\s*=\s*"\d.\d.\d.dev"', f'version = "{RELEASE_VERSION}"', pyproject.read_text())
-pyproject.write_text(pyproject_content)
 
-# create the distribution zip
-with ZipFile("wrappers.zip", "w") as zip_file:
-    zip_file.write("src/pyprojectx/wrapper/pw.py", "pw")
-    zip_file.write("src/pyprojectx/wrapper/pw.bat", "pw.bat")
+def cleanup_old_files():
+    Path("wrappers.zip").unlink(missing_ok=True)
+    Path(".release-changelog.md").unlink(missing_ok=True)
 
-# extract the first change log from CHANGELOG.md
-with Path("CHANGELOG.md").open() as changelog, Path(".changelog.md").open("w") as out:
-    fist_header = False
-    for line in changelog.readlines():
-        if not fist_header:
-            if line.startswith("###"):
-                fist_header = True
-            else:
-                continue
-        if fist_header and line.startswith("Release"):
-            break
-        out.write(line)
+
+def replace_version_in_files(release_version):
+    # replace __version__ in wrapper and pyproject.toml
+    pw = Path("src/pyprojectx/wrapper/pw.py")
+    pw.write_text(pw.read_text().replace("__version__", release_version))
+    pyproject = Path("pyproject.toml")
+    pyproject_content = re.sub(r'version\s*=\s*"\d.\d.\d.dev"', f'version = "{release_version}"', pyproject.read_text())
+    pyproject.write_text(pyproject_content)
+
+
+def zip_wrappers():
+    with ZipFile("wrappers.zip", "w") as zip_file:
+        zip_file.write("src/pyprojectx/wrapper/pw.py", "pw")
+        zip_file.write("src/pyprojectx/wrapper/pw.bat", "pw.bat")
+
+
+def generate_release_changelog():
+    # extract the first change log from CHANGELOG.md
+    with Path("CHANGELOG.md").open() as changelog, Path(".release-changelog.md").open("w") as out:
+        for index, line in enumerate(changelog):
+            if index == 0 and not line.startswith("### "):
+                message = (
+                    "First line of CHANGELOG.md must start with '### '. Did you forget to add a new release notes?"
+                )
+                raise RuntimeError(message)
+            if line.startswith("Release"):
+                break
+            out.write(line)
+
+
+def update_changelog(release_version):
+    date = datetime.now(tz=UTC).isoformat()[:10]
+    with Path("CHANGELOG.md").open() as file:
+        original_content = file.read()
+    with Path("CHANGELOG.md").open("w") as file:
+        file.write(f"Release v{release_version} ({date})\n")
+        file.write("----------------------------\n")
+        file.write(original_content)
+    subprocess.run(["git", "add", "CHANGELOG.md"], check=True)
+    subprocess.run(["git", "commit", "-m", f"Release v{release_version}"], check=True)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
