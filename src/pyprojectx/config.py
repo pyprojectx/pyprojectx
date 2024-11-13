d8e2a64f804fb730f5508f6ea23b1d753544dee8
Ivo Houbrechts <ivo@houbrechts-it.be>
2024-11-13 15:37:43 +0100

    127-easy-initialisation-of-a-uv-project (#128)
    * closes #126: make scripts_dir work with a venv
    
    * closes #126: docs
    
    * provide uv as default tool if no pyprojectx configuration found
    
    * docs
    
    ---------
    
    Co-authored-by: ihoubr <ivo.houbrechts@nike.com>

--- src/pyprojectx/config.py
+++ src/pyprojectx/config.py
@@ -10,6 +10,7 @@ import tomlkit
 from pyprojectx.wrapper.pw import BLUE, CYAN, RESET
 
 MAIN = "main"
+DEFAULT_TOOLS = ["uv"]
 PROJECT_DIR = "@PROJECT_DIR"
 DEFAULT_SCRIPTS_DIR = "bin"
 LOCK_FILE = "pw.lock"
@@ -29,17 +30,29 @@ class AliasCommand:
 class Config:
     """Encapsulates the PyprojectX config inside a toml file."""
 
+    # ruff: noqa: C901
     def __init__(self, toml_path: Path) -> None:
         """:param toml_path: The toml config file"""
         self._toml_path = toml_path
         try:
             with toml_path.open("rb") as f:
                 toml_dict = tomlkit.load(f)
+        except FileNotFoundError:
+            print(f"{CYAN}{toml_path}{BLUE} does not exist{RESET}", file=sys.stderr)
+            toml_dict = {}
         except Exception as e:
             raise Warning(f"Could not parse {toml_path}: {e}") from e
 
         project_path = toml_path.parent.absolute()
-        self._contexts = toml_dict.get("tool", {}).get("pyprojectx", {}).copy()
+        if not toml_dict.get("tool", {}).get("pyprojectx"):
+            print(
+                f"{BLUE}No configuration found, providing {CYAN}{MAIN}{BLUE} context with default tools:"
+                f" {CYAN}{DEFAULT_TOOLS}{RESET}",
+                file=sys.stderr,
+            )
+            self._contexts = {MAIN: DEFAULT_TOOLS}
+        else:
+            self._contexts = toml_dict.get("tool", {}).get("pyprojectx", {}).copy()
         self._aliases = self._contexts.pop("aliases", {})
         self.env = self._contexts.pop("env", {})
         self.prerelease = self._contexts.pop("prerelease", None)
